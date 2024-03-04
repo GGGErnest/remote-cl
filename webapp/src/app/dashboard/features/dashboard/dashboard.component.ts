2e2855822a644bffc1df973907ce110fe761ae77
Ernesto Pedrosa Alonso <ernestop891123@gmail.com>
2024-03-04 11:26:01 +0100

    feat: add fullscreen state behavior

--- webapp/src/app/dashboard/features/dashboard/dashboard.component.ts
+++ webapp/src/app/dashboard/features/dashboard/dashboard.component.ts
@@ -1,28 +1,37 @@
-import { AsyncPipe, NgFor } from '@angular/common';
+import { NgClass, NgFor } from '@angular/common';
 import {
   Component,
   OnInit,
-  inject
+  WritableSignal,
+  computed,
+  inject,
+  signal,
 } from '@angular/core';
 import { Server } from 'src/app/servers/data-access/server';
 import { ServersService } from 'src/app/servers/data-access/servers.service';
 import { StateService } from 'src/app/shared/data-access/state.service';
 import { ITerminalOptions } from 'xterm';
-import { TerminalTailComponent, TerminalTailState } from '../../../terminals/features/terminal-tail/terminal-tail.component';
+import {
+  TerminalTailComponent,
+  TerminalTailState,
+} from '../terminal-tail/terminal-tail.component';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { historySignal } from 'src/app/shared/utils/history-signal';
+
+type Terminal = {
+  id: string;
+  server: Server;
+  state: WritableSignal<TerminalTailState>;
+};
 
 @Component({
-    selector: 'dashboard',
-    templateUrl: './dashboard.component.html',
-    styleUrls: ['./dashboard.component.scss'],
-    standalone: true,
-    imports: [
-        NgFor,
-        TerminalTailComponent,
-        AsyncPipe,
-    ],
+  selector: 'dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss'],
+  standalone: true,
+  imports: [NgFor, TerminalTailComponent, NgClass],
 })
 export class DashboardComponent implements OnInit {
-  private _activeTerminals: string[] = [];
   public terminalOptions: ITerminalOptions = {
     cursorBlink: true,
     allowProposedApi: true,
@@ -30,19 +39,67 @@ export class DashboardComponent implements OnInit {
     macOptionIsMeta: true,
   };
   private _serversService = inject(ServersService);
-  public stateService = inject(StateService);
+  private _stateService = inject(StateService);
+  private _servers = toSignal(this._stateService.servers$, {
+    initialValue: [],
+  });
+  public terminals = computed(() => {
+    const terminalsData = new Map<string, Terminal>();
+    for (const server of this._servers()) {
+      for (const terminalId in server.runningShells) {
+        const terminalData: Terminal = {
+          id: terminalId,
+          server: server,
+          state: signal('normal'),
+        };
 
-  getTerminals(server: Server): string[] {
-    const terminalsOfServer = Object.values(server.runningShells);
-    this._activeTerminals.push(...terminalsOfServer);
-    return terminalsOfServer;
-  }
+        terminalsData.set(terminalId, terminalData);
+      }
+    }
+    return terminalsData;
+  });
+
+  public fullscreen = signal<string | undefined>(undefined);
 
   ngOnInit(): void {
     this._serversService.getServers().subscribe();
   }
 
-  onTerminalStateChange(terminalId:string , state: TerminalTailState) {
-    console.log('Terminal state change ', terminalId , state);
+  onTerminalStateChange(terminalId: string, state: TerminalTailState) {
+    for (const terminal of this.terminals().values()) {
+      if (terminalId === terminal.id) {
+        terminal.state.set(state);
+        continue;
+      }
+
+      if (state === 'fullscreen') {
+          terminal.state.set('minimized');
+          continue;
+      }
+
+      if (state === 'normal') {
+        if (terminal.state() === 'hidden') {
+          terminal.state.set('normal');
+          continue;
+        }
+
+        if (terminal.state() === 'fullscreen') {
+          terminal.state.set('normal');
+          continue;
+        }
+      }
+
+      if (state === 'minimized') {
+        if (terminal.state() === 'fullscreen') {
+          terminal.state.set('normal');
+          continue;
+        }
+
+        if (terminal.state() === 'fullscreen') {
+          terminal.state.set('normal');
+        }
+        continue;
+      }
+    }
   }
 }
